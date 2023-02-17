ab17d4ee5a605ace52f093ffc15389ecc1b28c00
David Lopez <lopezbnd@amazon.com>
2023-02-17 11:57:46 -0800

    chore: upgrade amplify cli

--- src/models/schema.js
+++ src/models/schema.js
@@ -69,6 +69,6 @@ export const schema = {
     },
     "enums": {},
     "nonModels": {},
-    "codegenVersion": "3.3.4",
+    "codegenVersion": "3.3.5",
     "version": "ef9b651791d1d68ed7fe60bc76c24b25"
 };
